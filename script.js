97144f4a7d9cc6f3fe62846b2cac7588d58c31ef
kevinparada99 <60240255+kevinparada99@users.noreply.github.com>
2021-06-08 08:57:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+
+//https://dribbble.com/shots/3581904-HEALTHEX-Day-01
+
+//SMOOTH SCROLL MENU
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {  
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+//FIXED MENU COLORS
+$(document).ready(function(){
+  $(window).trigger('scroll');
+  $(window).bind('scroll', function () {
+    var pixels = 50; //pixeles abajo
+    if ($(window).scrollTop() > pixels) {
+  $('.nav').addClass('fixed');
+      $('.menu a').css({"color":"#354757"});
+      $('.logo').css({"color":"#354757"});
+    } else {
+      $('.nav').removeClass('fixed');
+      $('.menu a').css({"color":"#BABCBD"});
+      $('.logo').css({"color":"#BABCBD"});
+    }
+  }); 
+
+
+}); 
+
+
+document.getElementById("Idioma").addEventListener('change', function () {
+  window.location = this.value;
+}, false);
+/*
+$(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e){
+    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
+    
+    //scroll con hash
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 1000, 'swing', function(){
+      window.location.hash = target;
+    });
+    
+   /* //scroll sin hash
+    $('html, body').animte({
+      'scrollTop': $targer.offset().top
+      }, 1000, 'swing');
+  });
+});*/
+
